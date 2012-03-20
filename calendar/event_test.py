eef6448c97ffd5b92817bcc72d442a295bc66f57
Danilo Cabello <danilo.cabello@gmail.com>
2012-03-20 00:39:51 -0300

    Start event_test refactoring

--- calendar/event_test.py
+++ calendar/event_test.py
@@ -16,16 +16,15 @@ class EventTest(unittest.TestCase):
         assert ev.start == start
         assert ev.end == end
 
-    def test_conflict_is_false(self):
+    def new_event(self, start_hour, start_minute, end_hour, end_minute):
         name = 'meeting'
-        start = datetime(2012, 3, 19, 18, 0)
-        end = datetime(2012, 3, 19, 19, 45)
-        ev = Event(name, start, end)
+        start = datetime(2012, 3, 19, start_hour, start_minute)
+        end = datetime(2012, 3, 19, end_hour, end_minute)
+        return Event(name, start, end)
 
-        name2 = 'nice lunch'
-        start2 = datetime(2012, 3, 19, 12, 0)
-        end2 = datetime(2012, 3, 19, 14, 0)
-        ev2 = Event(name2, start2, end2)
+    def test_conflict_is_false(self):
+        ev = self.new_event(18, 0, 19, 45)
+        ev2 = self.new_event(12, 0, 14, 0)
 
         assert ev.conflicts(ev2) == False
         assert ev2.conflicts(ev) == False
